3ba218e208e9e678f09ef5784c245477e73a567a
James Cahill <jcahill773@gmail.com>
2020-07-20 16:06:04 +0100

    [entrypoint] Log more information when running

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,12 +4,19 @@
 
 cd /home/container
 
+echo "ptero-entrypoint > Welcome! In Directory: '$(pwd)'"
+
 # Output Current Java Version
 java -version ## only really needed to show what version is being used. Should be changed for different applications
+
+
+echo "ptero-entrypoint > Replacing startup variables..."
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+echo "ptero-entrypoint > Starting server with: '$MODIFIED_STARTUP'"
 
 # Run the Server
 ${MODIFIED_STARTUP}
